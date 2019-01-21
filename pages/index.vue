a7f3b496a579b48f31351df891317719b35789a5
Matt Tyas <matthewtyas@hotmail.com>
2019-01-21 20:16:09 +0000

    very basic holding page

--- pages/index.vue
+++ pages/index.vue
@@ -1,42 +1,15 @@
 <template>
   <section class="container">
     <div>
-      <logo />
       <h1 class="title">
-        matthewtyas
+        Matt Tyas
       </h1>
       <h2 class="subtitle">
-        personal website
+        Principal designer
       </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
     </div>
   </section>
 </template>
-
-<script>
-import Logo from '~/components/Logo.vue'
-
-export default {
-  components: {
-    Logo
-  }
-}
-</script>
 
 <style>
 .container {
