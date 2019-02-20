40c7ad540304aad5ed37c3e14b8f83ad9eb13f8a
Matt Tyas <matthewtyas@hotmail.com>
2019-02-20 14:25:02 +0000

    mobile page width

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: pkg.name,
+    title: 'Matt Tyas - Service and interaction design. Product, team management and frontend engineering',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
